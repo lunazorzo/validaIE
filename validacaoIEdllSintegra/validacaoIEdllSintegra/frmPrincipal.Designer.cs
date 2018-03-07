74b6a847491fdc087112466dfbadeec8eba9387c
Allan Diego Luna Zorzo <allanluna@gmail.com>
2018-03-07 16:02:53 -0300

    Add files via upload

---
+++ validacaoIEdllSintegra/validacaoIEdllSintegra/frmPrincipal.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace validacaoIEdllSintegra
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.txtIE = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbEstados = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.btnValidar);
+            this.panel1.Controls.Add(this.txtIE);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cbEstados);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(355, 297);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidar.Location = new System.Drawing.Point(260, 86);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar.TabIndex = 4;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // txtIE
+            // 
+            this.txtIE.Location = new System.Drawing.Point(74, 58);
+            this.txtIE.Name = "txtIE";
+            this.txtIE.Size = new System.Drawing.Size(261, 22);
+            this.txtIE.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IE:";
+            // 
+            // cbEstados
+            // 
+            this.cbEstados.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbEstados.FormattingEnabled = true;
+            this.cbEstados.Items.AddRange(new object[] {
+            "AC - ACRE",
+            "AL - ALAGOAS",
+            "AM - AMAZONAS",
+            "AP - AMAPÁ",
+            "BA - BAHIA",
+            "CE - CEARÁ",
+            "DF - DISTRITO FEDERAL",
+            "ES - ESPÍRITO SANTO",
+            "GO - GOIÁS",
+            "MA - MARANHÃO",
+            "MG - MINAS GERAIS",
+            "MS - MATO GROSSO DO SUL",
+            "MT - MATO GROSSO",
+            "PA - PARÁ",
+            "PB - PARAÍBA",
+            "PE - PERNAMBUCO",
+            "PI - PIAUÍ",
+            "PR - PARANÁ",
+            "RJ - RIO DE JANEIRO",
+            "RN - RIO GRANDE DO NORTE",
+            "RO - RONDÔNIA",
+            "RR - RORAIMA",
+            "RS - RIO GRANDE DO SUL",
+            "SC - SANTA CATARINA",
+            "SE - SERGIPE",
+            "SP - SÃO PAULO",
+            "TO - TOCANTINS"});
+            this.cbEstados.Location = new System.Drawing.Point(74, 21);
+            this.cbEstados.Name = "cbEstados";
+            this.cbEstados.Size = new System.Drawing.Size(261, 24);
+            this.cbEstados.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Estado";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(14, 131);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(321, 151);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 321);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Validação IE dll Sintegra";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cbEstados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIE;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ validacaoIEdllSintegra/validacaoIEdllSintegra/frmPrincipal.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+namespace validacaoIEdllSintegra
+{
+    public partial class frmPrincipal : Form
+    {
+        [DllImport("DllInscE32.dll")]
+        public static extern int ConsisteInscricaoEstadual(string cInsc, string cUF);
+        String sgEstado;
+        String ieLimpa;
+        Int32 retornoIE;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {            
+            try
+            {
+                
+                bool prossegue = true;
+                if (cbEstados.SelectedIndex == -1 && prossegue)
+                {
+                    MessageBox.Show("É necessário informar o Estado!");
+                    cbEstados.Focus();
+                    prossegue = false;
+                }
+                if (txtIE.Text.Equals(string.Empty) && prossegue)
+                {
+                    MessageBox.Show("É necessário informar a IE!");
+                    cbEstados.Focus();
+                    prossegue = false;
+                }
+                if (prossegue == true)
+                {
+                    ieLimpa = OnlyNumbers(txtIE.Text);
+                    // AC - ACRE 
+                    if (cbEstados.SelectedIndex == 0) { sgEstado = "AC"; }
+                    //AL - ALAGOAS 
+                    if (cbEstados.SelectedIndex == 1) { sgEstado = "AL"; }
+                    //AM - AMAZONAS                                
+                    if (cbEstados.SelectedIndex == 2) { sgEstado = "AM"; }
+                    //AP - AMAPÁ                                   
+                    if (cbEstados.SelectedIndex == 3) { sgEstado = "AP"; }
+                    //BA - BAHIA                                   
+                    if (cbEstados.SelectedIndex == 4) { sgEstado = "BA"; }
+                    //CE - CEARÁ                                   
+                    if (cbEstados.SelectedIndex == 5) { sgEstado = "CE"; }
+                    //DF - DISTRITO FEDERAL                        
+                    if (cbEstados.SelectedIndex == 6) { sgEstado = "DF"; }
+                    //ES - ESPÍRITO SANTO                          
+                    if (cbEstados.SelectedIndex == 7) { sgEstado = "ES"; }
+                    //GO - GOIÁS                                   
+                    if (cbEstados.SelectedIndex == 8) { sgEstado = "GO"; }
+                    //MA - MARANHÃO                                
+                    if (cbEstados.SelectedIndex == 9) { sgEstado = "MA"; }
+                    //MG - MINAS GERAIS 
+                    if (cbEstados.SelectedIndex == 10) { sgEstado = "MG"; }
+                    //MS - MATO GROSSO DO SUL                       
+                    if (cbEstados.SelectedIndex == 11) { sgEstado = "MS"; }
+                    //MT - MATO GROSSO                              
+                    if (cbEstados.SelectedIndex == 12) { sgEstado = "MT"; }
+                    //PA - PARÁ                                     
+                    if (cbEstados.SelectedIndex == 13) { sgEstado = "PA"; }
+                    //PB - PARAÍBA                                  
+                    if (cbEstados.SelectedIndex == 14) { sgEstado = "PB"; }
+                    //PE - PERNAMBUCO                               
+                    if (cbEstados.SelectedIndex == 15) { sgEstado = "PE"; }
+                    //PI - PIAUÍ                                    
+                    if (cbEstados.SelectedIndex == 16) { sgEstado = "PI"; }
+                    //PR - PARANÁ                                   
+                    if (cbEstados.SelectedIndex == 17) { sgEstado = "PR"; }
+                    //RJ - RIO DE JANEIRO                           
+                    if (cbEstados.SelectedIndex == 18) { sgEstado = "RJ"; }
+                    //RN - RIO GRANDE DO NORTE                      
+                    if (cbEstados.SelectedIndex == 19) { sgEstado = "RN"; }
+                    //RO - RONDÔNIA                                 
+                    if (cbEstados.SelectedIndex == 20) { sgEstado = "RO"; }
+                    //RR - RORAIMA                                  
+                    if (cbEstados.SelectedIndex == 21) { sgEstado = "RR"; }
+                    //RS - RIO GRANDE DO SUL                        
+                    if (cbEstados.SelectedIndex == 22) { sgEstado = "RS"; }
+                    //SC - SANTA CATARINA                           
+                    if (cbEstados.SelectedIndex == 23) { sgEstado = "SC"; }
+                    //SE - SERGIPE                                  
+                    if (cbEstados.SelectedIndex == 24) { sgEstado = "SE"; }
+                    //SP - SÃO PAULO                                
+                    if (cbEstados.SelectedIndex == 25) { sgEstado = "SP"; }
+                    //TO - TOCANTINS                                
+                    if (cbEstados.SelectedIndex == 26) { sgEstado = "TO"; }
+
+                    retornoIE = ConsisteInscricaoEstadual(ieLimpa, sgEstado);
+                    if (retornoIE == 0)
+                    {
+                        MessageBox.Show("Incrição Estadual válida", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else if (retornoIE == 1)
+                    {
+                        MessageBox.Show("Incrição Estadual inválida", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                    else if (retornoIE == 2)
+                    {
+                        MessageBox.Show("Parâmetro inválido", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    txtIE.Clear();
+                    cbEstados.SelectedIndex = -1;
+                    cbEstados.Focus();
+                }
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro ao válidar IE! \n" + erro.Message);
+            }
+        }
+        public string OnlyNumbers(string toNormalize)
+        {
+            string resultString = string.Empty;
+            Regex regexObj = new Regex(@"[^\d]");
+            resultString = regexObj.Replace(toNormalize, "");
+            return resultString;
+        } 
+    }
+}
